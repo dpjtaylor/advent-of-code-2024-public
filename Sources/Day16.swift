1491e84d1aca55df0c5deab855f3d4d17272c9ea
David Taylor <dpjtaylor@protonmail.com>
2024-12-23 10:14:55 +0000

    day 16 - part 2 + part 1 rewrite

--- Sources/Day16.swift
+++ Sources/Day16.swift
@@ -4,88 +4,170 @@ import Collections
 public enum Day16 {
     public enum Part1 {
         static func solve(_ data: String) -> Int {
-            data.grid.reindeerPaths()
+            data.grid.reindeerPathsPart1()
         }
     }
 
     public enum Part2 {
-        static func solve(_ data: String) -> Int {
-           -1
+        static func solve(_ data: String, minScore: Int) -> Int {
+            data.grid.reindeerPathsPart2(minScore: minScore)
         }
+    }
+}
+
+struct ReindeerPath: Hashable, Equatable, Comparable {
+    let step: ReindeerPathStep
+    let score: Int
+    let visited: [Coordinates]
+
+    static func < (lhs: ReindeerPath, rhs: ReindeerPath) -> Bool {
+        lhs.score < rhs.score
+    }
+}
+
+struct ReindeerPathStep: Equatable, Hashable {
+    let coordinates: Coordinates
+    let direction: Direction
+
+    init(coordinates: Coordinates, direction: Direction) {
+        self.coordinates = coordinates
+        self.direction = direction
+    }
+
+    var nextSteps: [ReindeerPathStep] {
+        let nextDirection: [Direction] = switch direction {
+        case .north: [.north, .east, .west]
+        case .south: [.south, .east, .west]
+        case .east: [.east, .north, .south]
+        case .west: [.west, .north, .south]
+        }
+
+        return nextDirection.map { direction in
+            switch direction {
+            case .north: move(.north)
+            case .south: move(.south)
+            case .east: move(.east)
+            case .west: move(.west)
+            }
+        }
+    }
+
+    func move(_ direction: Direction) -> ReindeerPathStep {
+        ReindeerPathStep(coordinates: coordinates.move(direction), direction: direction)
     }
 }
 
 extension Array where Element == [Character] {
-    func reindeerPaths(debug: Bool = false) -> Int {
+    func reindeerPathsPart1(debug: Bool = false) -> Int {
         let start = findCoordinates(for: "S").first!
         let end = findCoordinates(for: "E").first!
-        let startStep = DirectedStep(direction: .east, to: start)
+        let startStep = ReindeerPathStep(coordinates: start, direction: .east)
+        let startPath = ReindeerPath(step: startStep, score: 0, visited: [start])
 
-        var pathQueue = Heap<DirectedStep>()
-        pathQueue.insert(startStep)
-        var foundEnd = false
-        var step = 0
-        while !foundEnd {
-            let currentStep = pathQueue.popMin()!
-            var nextSteps = nextStepOptions(from: currentStep)
+        var optimalPaths: [ReindeerPath] = []
+        var pathQueue = Heap<ReindeerPath>()
+        pathQueue.insert(startPath)
+        var visited: Set<ReindeerPathStep> = []
+        var minScore = Int.max
+        while !pathQueue.isEmpty {
+            let currentPath = pathQueue.popMin()!
 
-            for nextStep in nextSteps {
-                if pathQueue.unordered.contains(where: { $0.coordinates == nextStep.coordinates && $0.direction == nextStep.direction } ) {
-                    nextSteps.removeAll { $0 == nextStep }
-                } // avoid looping
+            let currentStep = currentPath.step
+            if visited.contains(currentStep) {
+                continue
             }
-            pathQueue.insert(contentsOf: nextSteps)
-            let min = pathQueue.min!
-            if debug {
-                print("\(step): \(min.direction.displayChar) \(min.coordinates.x), \(min.coordinates.y), score: \(min.score)")
+            visited.insert(currentStep)
+
+            if currentStep.coordinates == end {
+                if currentPath.score <= minScore {
+                    minScore = currentPath.score
+                    optimalPaths.append(currentPath)
+                }
+                continue
             }
-            if pathQueue.unordered.contains(where: { $0.coordinates == end }) {
-                foundEnd = true
+
+            for nextPath in nextPaths(from: currentPath) {
+                pathQueue.insert(nextPath)
             }
-            step += 1
         }
-        return pathQueue.unordered.filter { $0.coordinates == end }.score
+
+        if debug {
+            for path in optimalPaths {
+                var gridCopy = self
+                for coord in path.visited {
+                    gridCopy[coord.y][coord.x] = "+"
+                }
+                gridCopy.debug()
+                print("")
+            }
+            print("optimal path score: \(minScore)")
+        }
+
+        return minScore
     }
 
-    func nextStepOptions(from step: DirectedStep, stopAtEnd: Bool = false) -> [DirectedStep] {
-        let x = step.coordinates.x
-        let y = step.coordinates.y
+    // Struggled with Part 2 for a few days with solutions that would take days to run, and ended up translating this python example here, which solves the problem very quickly. I then went back and re-wrote part 1 🙌
+    // https://github.com/seapagan/aoc-2024/blob/main/16/main.py
+    func reindeerPathsPart2(debug: Bool = false, minScore: Int) -> Int {
+        let start = findCoordinates(for: "S").first!
+        let end = findCoordinates(for: "E").first!
+        let startStep = ReindeerPathStep(coordinates: start, direction: .east)
+        let startPath = ReindeerPath(step: startStep, score: 0, visited: [start])
 
-        if stopAtEnd && self[y][x] == "E" {
-            return []
+        var pathQueue = Heap<ReindeerPath>()
+        pathQueue.insert(startPath)
+        var stepScoreLookup: [ReindeerPathStep: Int] = [startStep: 0]
+
+        var optimalTiles: Set<Coordinates> = []
+
+        while !pathQueue.isEmpty {
+            let currentPath = pathQueue.popMin()!
+
+            if currentPath.score > minScore {
+                continue
+            }
+
+            let currentStep = currentPath.step
+            if let existingStepScore = stepScoreLookup[currentStep],
+               existingStepScore < currentPath.score {
+                continue
+            }
+            stepScoreLookup[currentStep] = currentPath.score
+
+            if currentStep.coordinates == end,
+               currentPath.score == minScore {
+                optimalTiles = optimalTiles.union(Set(currentPath.visited))
+                continue
+            }
+
+            for nextPath in nextPaths(from: currentPath) {
+                pathQueue.insert(nextPath)
+            }
         }
 
-        let north = self[y - 1][x]
-        let south = self[y + 1][x]
-        let east = self[y][x + 1]
-        let west = self[y][x - 1]
+        if debug {
+            var gridCopy = self
+            for tile in optimalTiles {
+                gridCopy[tile.y][tile.x] = "O"
+            }
+            gridCopy.debug()
+            print("optimal tiles: \(optimalTiles.count)")
+        }
+        return optimalTiles.count
+    }
 
-        let from = step.coordinates
-        var nextSteps: [DirectedStep] = []
+    func nextPaths(from path: ReindeerPath) -> [ReindeerPath] {
+        nextStepOptions(from: path.step).map { nextStep in
+            let rotationScore = nextStep.direction == path.step.direction ? 0 : 1000
+            let newScore = path.score + 1 + rotationScore
+            return ReindeerPath(step: nextStep, score: newScore, visited: path.visited + [nextStep.coordinates])
+        }
+    }
+
+    func nextStepOptions(from step: ReindeerPathStep) -> [ReindeerPathStep] {
         let doNotVisit: [Character] = ["#", "S"] // avoid walls and revisiting 'S'
-        if !doNotVisit.contains(north) {
-            let score = step.score(nextDirection: .north)
-            nextSteps.append(DirectedStep(direction: .north, from: from, to: Coordinates(x: x, y: y - 1), score: score))
-        }
-        if !doNotVisit.contains(south) {
-            let score = step.score(nextDirection: .south)
-            nextSteps.append(DirectedStep(direction: .south, from: from, to: Coordinates(x: x, y: y + 1), score: score))
-        }
-        if !doNotVisit.contains(east) {
-            let score = step.score(nextDirection: .east)
-            nextSteps.append(DirectedStep(direction: .east, from: from, to: Coordinates(x: x + 1, y: y), score: score))
-        }
-        if !doNotVisit.contains(west){
-            let score = step.score(nextDirection: .west)
-            nextSteps.append(DirectedStep(direction: .west, from: from, to: Coordinates(x: x - 1, y: y), score: score))
-        }
-        return nextSteps .filter { $0.direction != step.direction.reversed } // avoid backtracking
-    }
-}
 
-extension DirectedStep {
-    func score(nextDirection: Direction) -> Int {
-        let rotations = direction.rotations(to: nextDirection)
-        return score + rotations * 1000 + 1
+        return step.nextSteps.filter { step in !doNotVisit.contains(self[step.coordinates.y][step.coordinates.x])
+        }
     }
 }
